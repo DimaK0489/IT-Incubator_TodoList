8edaa22e5011ec4335754764225d5dba8bdb9dd9
Dima_Kaleinik <dim040489@gmail.com>
2021-05-21 11:56:56 +0300

    fix bag, Log Out

--- src/features/Todolists/Login/authReducer.ts
+++ src/features/Todolists/Login/authReducer.ts
@@ -28,6 +28,7 @@ export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch<ActionsTyp
         .then((res) => {
             if (res.data.resultCode === 0) {
                 dispatch(setIsLoggedInAC(true))
+                dispatch(setAppStatusAC('succeeded'))
             } else {
                 handleServerAppError(res.data, dispatch)
             }
@@ -42,6 +43,7 @@ export const logOutTC = () => (dispatch: Dispatch<ActionsType>) => {
         .then((res) => {
             if (res.data.resultCode === 0) {
                 dispatch(setIsLoggedInAC(false))
+                dispatch(setAppStatusAC('succeeded'))
             } else {
                 handleServerAppError(res.data, dispatch)
             }
