f85ed44574acc917a7c03c07db581663f4fb8862
Dima_Kaleinik <dim040489@gmail.com>
2021-06-15 15:05:15 +0300

    Updates

---
+++ precache-manifest.cf966a6ad854d440f231324e34f4d0a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fccef67b56d4a5b57d0f050de7e0f4cb",
+    "url": "/IT-Incubator_TodoList/index.html"
+  },
+  {
+    "revision": "22b7f3e0dd14f57e1fd2",
+    "url": "/IT-Incubator_TodoList/static/css/main.37306adb.chunk.css"
+  },
+  {
+    "revision": "245cbefa2bba64198a92",
+    "url": "/IT-Incubator_TodoList/static/js/2.6480e24e.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/IT-Incubator_TodoList/static/js/2.6480e24e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b7f3e0dd14f57e1fd2",
+    "url": "/IT-Incubator_TodoList/static/js/main.dfdb7144.chunk.js"
+  },
+  {
+    "revision": "358c808ca72468887d6a",
+    "url": "/IT-Incubator_TodoList/static/js/runtime-main.b5e5cbaa.js"
+  }
+]);
