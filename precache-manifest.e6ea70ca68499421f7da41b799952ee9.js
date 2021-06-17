bbd6193b7e6001c3435bd78e51cbcd0227ac3922
Dima_Kaleinik <dim040489@gmail.com>
2021-06-17 17:52:38 +0300

    Updates

--- precache-manifest.e6ea70ca68499421f7da41b799952ee9.js
+++ precache-manifest.e6ea70ca68499421f7da41b799952ee9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fccef67b56d4a5b57d0f050de7e0f4cb",
+    "revision": "750f1e37516aa26f39b2c5c3b5ac2097",
     "url": "/IT-Incubator_TodoList/index.html"
   },
   {
-    "revision": "22b7f3e0dd14f57e1fd2",
+    "revision": "2d73cece85d39e365f5b",
     "url": "/IT-Incubator_TodoList/static/css/main.37306adb.chunk.css"
   },
   {
-    "revision": "245cbefa2bba64198a92",
-    "url": "/IT-Incubator_TodoList/static/js/2.6480e24e.chunk.js"
+    "revision": "8896f1eb573ba1be0221",
+    "url": "/IT-Incubator_TodoList/static/js/2.4809b95e.chunk.js"
   },
   {
     "revision": "d2266803016540034565984c8bb59069",
-    "url": "/IT-Incubator_TodoList/static/js/2.6480e24e.chunk.js.LICENSE.txt"
+    "url": "/IT-Incubator_TodoList/static/js/2.4809b95e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "22b7f3e0dd14f57e1fd2",
-    "url": "/IT-Incubator_TodoList/static/js/main.dfdb7144.chunk.js"
+    "revision": "2d73cece85d39e365f5b",
+    "url": "/IT-Incubator_TodoList/static/js/main.a9164e37.chunk.js"
   },
   {
     "revision": "358c808ca72468887d6a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/IT-Incubator_TodoList/precache-manifest.cf966a6ad854d440f231324e34f4d0a4.js"
+  "/IT-Incubator_TodoList/precache-manifest.e6ea70ca68499421f7da41b799952ee9.js"
 );
 
 self.addEventListener('message', (event) => {
